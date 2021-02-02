706e0106261f6684f33b3d48a712366ebad40f1d
Michał Noszczyk <noszczykmichal@wp.pl>
2021-02-03 00:33:17 +0100

    modified controls of the counter to use the redux

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,6 +3,35 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action) => {
+
+    if (action.type === 'INCREMENT') {
+        return {
+            ...state,
+            counter: state.counter + 1
+        }
+    }
+
+    if (action.type === 'DECREMENT') {
+        return {
+            ...state,
+            counter: state.counter - 1
+        }
+    }
+
+    if (action.type === 'ADD') {
+        return {
+            ...state,
+            counter: state.counter + 5
+        }
+    }
+
+    if (action.type === 'SUBTRACT') {
+        return {
+            ...state,
+            counter: state.counter - 5
+        }
+    }
+
     return state;
 };
 
